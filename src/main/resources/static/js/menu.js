d0736da503241bed96bc9f57e9a4342dea503b16
Y Sato <mentol310@gmail.com>
2016-06-13 23:31:03 +0900

    Add menu.js

---
+++ src/main/resources/static/js/menu.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('#menuOne, #menuTwo, #menuThree').on('show.bs.collapse', function() {
+		$('a[href="#' + this.id + '"]').find('span.glyphicon-chevron-down').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+	})
+	.on('hide.bs.collapse', function() {
+		$('a[href="#' + this.id + '"]').find('span.glyphicon-chevron-up').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+	});
+	$('a[href="#menuOne"], a[href="#menuTwo"], a[href="#menuThree"]').on('click', function(event) {
+		event.preventDefault();
+	});
+});
